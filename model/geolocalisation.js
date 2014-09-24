4b1d2023ffc4cdaf95ac8dd738cdda0c36830597
neguewatt <david.nzi@epsi.fr>
2014-09-24 16:53:19 +0200

    geoloc

--- model/geolocalisation.js
+++ model/geolocalisation.js
@@ -1,1 +1,11 @@
 <script type = 'text/javascript' src = 'externe/jquery.js'></script>
+
+if(navigator.geolocation)
+{
+	navigator.geolocation.getCurrentPosition(function(position)
+	{
+		alert("latitude : " + position.coords.latitude + ", longitude : " + position.coords.longitude);
+	});
+}
+else
+	alert("votre navigateur ne peux afficher la geolocalisation");
---
+++ view/geolocview.js
@@ -0,0 +1,1 @@
+geolocview.js
