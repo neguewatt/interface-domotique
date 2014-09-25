183210904a4ddc9dfb32e6d61194e6267bd41bff
nozkrew <thomas.maire1@hotmail.fr>
2014-09-25 15:30:28 +0200

    perfectionnement date

--- view/date.js
+++ view/date.js
@@ -16,5 +16,7 @@ include("../controller/date.js", divDate);
 
 function divDate(){
 	var tabDate = avoirDate();
-	$("#date").html(tabDate.lHeure + " : " + tabDate.laMinute + " " + tabDate.leJourSemaine + " " + tabDate.leJour + "/" + tabDate.leMois + "/" + tabDate.lAnnee);
+	$("#date").html(tabDate.leJourSemaine + " " + tabDate.leJour + "/" + tabDate.leMois + "/" + tabDate.lAnnee + ", " + tabDate.lHeure + ":" + tabDate.laMinute);
 }
+
+setInterval(function(){divDate()}, 1000);
