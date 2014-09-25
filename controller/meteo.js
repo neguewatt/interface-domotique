cbd6b75af10e28e8e09cb7a928c5a9c1d8eb6eaa
nozkrew <thomas.maire1@hotmail.fr>
2014-09-25 17:52:59 +0200

    modif fichier

--- controller/meteo.js
+++ controller/meteo.js
@@ -1,20 +1,7 @@
-function include(url, callback){
-
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = url ;
-
-	if (callback) {
-		script.onload = function(){
-			callback();
-		}
-	}
-	document.getElementsByTagName("head")[0].appendChild(script);
-}
-
-//inclure model/meteo
-include("../model/meteo.js", getMeteo);
-
 function getMeteo(){
-
+	var latitude = $('#geoloc > #latitude').val();
+	var longitude = $('#geoloc > #longitude').val();
+	var laMeteo = new meteo(0,0,0,0,0,0,0,0,0);
+	laMeteo.getMeteo(latitude, longitude);
+	return laMeteo;
 }
--- view/meteo.js
+++ view/meteo.js
@@ -1,19 +1,5 @@
-function include(url, callback){
-
-	var script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = url ;
-
-	if (callback) {
-		script.onload = function(){
-			callback();
-		}
-	}
-	document.getElementsByTagName("head")[0].appendChild(script);
-}
-include("../controller/meteo.js", divmeteo);
-
 function divmeteo()
 {
-	
+	var laMeteo = getMeteo();
+	alert(laMeteo.temperature);
 }
