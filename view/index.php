7316138a7071cac8bf68282fe8b1c238a3a6ab32
neguewatt <david.nzi@epsi.fr>
2014-09-26 00:06:48 +0200

    css optimisation index

--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@
 
 	<head>
 		<meta charset="utf-8">
-<<<<<<< HEAD
+
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Accueil interface de domotique</title>
 	<link rel="stylesheet" href="css/style.css" />
@@ -12,7 +12,7 @@
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile.structure-1.4.3.min.css" />
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
-=======
+
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Accueil interface de domotique</title>
 		<link rel="stylesheet" href="themes/domotique.min.css" />
@@ -20,7 +20,7 @@
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile.structure-1.4.3.min.css" />
 		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
->>>>>>> FETCH_HEAD
+
 	</head>
 
 	<body>
@@ -28,16 +28,18 @@
 			<div data-role="header" data-position="inline">
 				<h1><img src="images/home.png" alt="home" class="imagehome">HOME</h1>
 			</div>
-
-			<div id = 'date'></div>
-			<div id = 'heure'></div>
-			<div id = 'geoloc'>
-				<input type = 'hidden' id = 'latitude'></input>
-				<input type = 'hidden' id = 'longitude'></input>	
-				<div id = 'erreur'></div>		
-			</div>
-			<div id = 'meteo'></div>
-
+			
+				<div class= "carrecentral">
+				<div class= "centreheure" id = 'heure'></div>
+				<div class= "centredate" id = 'date'></div>
+				<div id = 'geoloc'>
+					<input type = 'hidden' id = 'latitude'></input>
+					<input type = 'hidden' id = 'longitude'></input>	
+					<div id = 'erreur'></div>		
+				</div>
+				<div class= "centremeteo" id = 'meteo'></div>
+				</div>
+			
 			<!--	Inclusion du javascript	-->
 			<script type="text/javascript" src = "../model/externe/jquery.js" ></script>
 			<?php 
