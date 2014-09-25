7e603c21c80256ce6c769a0ecdfe4377a167d737
nozkrew <thomas.maire1@hotmail.fr>
2014-09-25 15:59:49 +0200

    model/meteo

--- model/meteo.js
+++ model/meteo.js
@@ -1,20 +1,32 @@
+function meteo(){
 
+	private	ville, //location
+			temperature, //temp_c
+			image,
+			uneDescription, //weather
+			altImage, //unTitle
+			directionVent,
+			vitesseVent,
+			tempRessentie,
+			visibilite;
 
-function getMeteo()
-{
-	$.ajax({
-		url : "http://api.wunderground.com/api/3f0376e269a62937/geolookup/conditions/lang:FC/q/"+maLatitude+","+maLongitude+".json",
-		dataType : "jsonp",
-		success : function(parsed_json) {
-			var location = parsed_json['location']['city'];
-			var temp_c = parsed_json['current_observation']['temp_c'] + "°C";
-			var uneimage = parsed_json['current_observation']['icon_url'];
-			var untitle = parsed_json['current_observation']['icon'];
-			var directionVent = parsed_json['current_observation']['wind_dir'];
-			var vitesseVent= parsed_json['current_observation']['wind_kph']+ " km/h";
-			var weather = parsed_json['current_observation']['weather'];
-			var tempRessentie = parsed_json['current_observation']['feelslike_c'] + "°C";
-			var visibilityKM = parsed_json['current_observation']['visibility_km'] + " km";								
-			}
-	});
+	function getMeteo(p_latitue, p_longitude)
+	{
+		$.ajax({
+			url : "http://api.wunderground.com/api/3f0376e269a62937/geolookup/conditions/lang:FC/q/"+p_latitue+","+p_longitude+".json",
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				this.ville = parsed_json['location']['city'];
+				this.temperature = parsed_json['current_observation']['temp_c'] + "°C";
+				this.image = parsed_json['current_observation']['icon_url'];
+				this.altImage = parsed_json['current_observation']['icon'];
+				this.directionVent = parsed_json['current_observation']['wind_dir'];
+				this.vitesseVent= parsed_json['current_observation']['wind_kph']+ " km/h";
+				this.uneDescription = parsed_json['current_observation']['weather'];
+				this.tempRessentie = parsed_json['current_observation']['feelslike_c'] + "°C";
+				this.visibilite = parsed_json['current_observation']['visibility_km'] + " km";								
+				}
+		});
+	}
+
 }
