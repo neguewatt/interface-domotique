03611b5bf16541bd7548937c0c015fbc6747e832
neguewatt <david.nzi@epsi.fr>
2014-09-25 19:24:47 +0200

    css

--- view/index.php
+++ view/index.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Accueil interface de domotique</title>
+	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="themes/domotique.min.css" />
 	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile.structure-1.4.3.min.css" />
@@ -15,7 +16,7 @@
 	<body>
 		<div data-role="page" data-theme="a">
 			<div data-role="header" data-position="inline">
-				<h1><img src="images/home.png" alt="home" id="imagehome">HOME</h1>
+				<h1><img src="images/home.png" alt="home" class="imagehome">HOME</h1>
 			</div>
 
 			<div id = 'date'></div>
