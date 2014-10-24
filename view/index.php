a5bcdea8a22b31cd6099195d54700e73bbb739bf
neguewatt <david.nzi@epsi.fr>
2014-10-24 11:16:24 +0200

    test popup pour les volets ??? OUI / NON???

--- view/index.php
+++ view/index.php
@@ -15,9 +15,9 @@
 
 	<body>
 		<div data-role="page" data-theme="a" id="imageecran">
-			<div data-role="header" data-position="fixed">
+			<!--<div data-role="header" data-position="fixed">
 				<h1><img src="images/home.png" alt="home" class="imageheader">HOME</h1>
-			</div>
+			</div>!-->
 
 
 
@@ -58,17 +58,18 @@
 							        <h3>Tappez le code pour allumer / desactiver l'alarme.</h3>
 							        <div id ="boutonchiffre">
 								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >1</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >3</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >2</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >3</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >2</a>
 								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >4</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >6</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >5</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >6</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >5</a>
 								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >7</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >9</a>
-								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right" >8</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >9</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >8</a>
 								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left">0</a>
+								        <a href="#" data-role="button" data-rel="back" style="float : right; height: 15px;width: 54px;left: 2px" >valider</a> 
 							   	 	</div>
-							        <a href="#" data-role="button" data-inline="true" data-rel="back" data-theme="c">annuler</a> 
+							        <a href="#" data-role="button" data-rel="back" data-theme="c" class="boutonannuleralarm">annuler</a>
 							    </div>
 							</div>
 						</div>
@@ -122,14 +123,36 @@
 
 				<div id="carrebasgauche">
 					<div>	
-						<a href="volets.html" data-role="button" id="boutonhautcarrebas" data-transition="pop">
-						<img src="images/fenetre.png" class="imagefenetre"><p  class="textboutonhaut">Gestion des volets</p>
+						<a href="#popupDialog" data-rel="popup" data-position-to="window"  data-inline="false" data-transition="fade" data-theme="a" data-role="button" id="boutonhautcarrebas">
+							<img src="images/fenetre.png" class="imagefenetre"><p  class="textboutonhaut">Gestion des volets</p>
 						</a>
+							<div data-role="popup" id="popupDialog" data-overlay-theme="a" data-theme="c" data-dismissible="false" style="max-width:400px;" class="ui-corner-all">
+							    <div data-role="header" data-theme="a" class="ui-corner-top">
+							        <h1>Gestion de l'alarme</h1>
+							    </div>
+							    <div data-role="content" data-theme="d" class="ui-corner-bottom ui-content">
+							        <h3>Tappez le code pour allumer / desactiver l'alarme.</h3>
+							        <div id ="boutonchiffre">
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >1</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >3</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >2</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >4</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >6</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >5</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left" >7</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 4px" >9</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : right; left: 2px" >8</a>
+								        <a href="#" data-role="button" id="tailleboutonchiffre" style="float : left">0</a>
+								        <a href="#" data-role="button" data-rel="back" style="float : right; height: 15px;width: 54px;left: 2px" >valider</a> 
+							   	 	</div>
+							        <a href="#" data-role="button" data-rel="back" data-theme="c" class="boutonannuleralarm">annuler</a>
+							    </div>
+							</div>
 					</div>
 					<div>
-						<input type="checkbox" name="chambre "style="margin-top: 30px">
-						<input type="checkbox" name="salon "style="margin-top: 75px">
-						<input type="checkbox" name="cuisine "style="margin-top: 115px">
+						<input type="checkbox" name="chambre "style="margin-top: 10px">
+						<input type="checkbox" name="salon "style="margin-top: 55px">
+						<input type="checkbox" name="cuisine "style="margin-top: 98px">
 					</div>
 					<div class="tableaupieces">		
 						<ul id="two" class="tablist-content" data-role="listview" data-inset="true">
@@ -148,8 +171,8 @@
 
 					<div class="bascarrevolets">
 						<div  data-inline="true">
-							<img id="imagevoletsbouton" src="images/volets-ouvert.png">
-							<p id="tailletextevolets">Ouverture/Fermeture</p>
+							<p id="tailletextetitre">Gestion de tous les volets</p>
+							<p id="tailletexteperso">Ouverture/Fermeture</p>
 							<a href=""><img class="imageflechehaut" type="image" src="images/flechehaut.png"></a>
 							<a href=""><img class="imagefleche" type="image" src="images/stop.png"></a>
 							<a href=""><img class="imagefleche" type="image" src="images/flechebas.png"></a>
@@ -157,11 +180,40 @@
 					</div>
 				</div>
 
-				
+								<!--     	MODULE DE LA GESTION DES APP EXTERIEUR		 -->
+
+				<div id="carrebasdroite">
+					<div>	
+						<a href="volets.html" data-role="button" id="boutonhautcarrebas" data-transition="pop">
+						<p  class="textboutonhaut">Gestion de l'exterieur</p>
+						</a>
+					</div>
+						<div  data-inline="true">	
+							<p id="tailletextetitre">Porte du garage</p>
+							<p id="tailletexteperso">Ouverture/Fermeture</p>
+							<a href="#"><img class="imageflechehaut" type="image" src="images/flechehaut.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/stop.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/flechebas.png"></a>
+						</div>
+						<div  data-inline="true">
+							<p id="tailletextetitre">Portail</p>
+							<p id="tailletexteperso">Ouverture/Fermeture</p>
+							<a href="#"><img class="imageflechehaut" type="image" src="images/flechehaut.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/stop.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/flechebas.png"></a>
+						</div>
+						<div  data-inline="true">
+							<p id="tailletextetitre">Porte d'entrée</p>
+							<p id="tailletexteperso">Ouverture/Fermeture</p>
+							<a href="#"><img class="imageflechehaut" type="image" src="images/flechehaut.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/stop.png"></a>
+							<a href="#"><img class="imagefleche" type="image" src="images/flechebas.png"></a>
+						</div>
+				</div>
 
 				<!--     	MODULE DE LA GESTION DES LUMIERES	 -->
 
-				<div id ="carrebasdroite">
+				<div id ="carrecentralbas">
 					<div>
 						<a href="lumiere.html" data-role="button" id="boutonhautcarrebas" data-transition="pop">
 						<img src="images/ampoule.png" class="imageampoule"><p  class="textboutonhaut">Gestion des Lumières</p>
@@ -178,45 +230,8 @@
 					</div>
 				</div>
 
-				<!--     	MODULE DE LA GESTION DES VOLETS		 -->
 
-				<div id="carrecentralbas">
-					<div>	
-						<a href="volets.html" data-role="button" id="boutonhautcarrebas" data-transition="pop">
-						<img src="images/fenetre.png" class="imagefenetre"><p  class="textboutonhaut">Gestion des volets</p>
-						</a>
-					</div>
-					<div>
-						<input type="checkbox" name="chambre "style="margin-top: 30px">
-						<input type="checkbox" name="salon "style="margin-top: 75px">
-						<input type="checkbox" name="cuisine "style="margin-top: 115px">
-					</div>
-					<div class="tableaupieces">		
-						<ul id="two" class="tablist-content" data-role="listview" data-inset="true">
-					        <li><a href="#popupUndismissible" data-transition="turn" data-role="button" data-inline="true" data-rel="popup" data-dismissible="false">Chambre</a></li>
-					        <li><a href="#popupUndismissible" data-transition="turn" data-role="button" data-inline="true" data-rel="popup" data-dismissible="false">Salon</a></li>
-					        <li><a href="#popupUndismissible" data-transition="turn" data-role="button" data-inline="true" data-rel="popup" data-dismissible="false">Cuisine</a></li>
-					    </ul>
-					</div>
-					<div data-role="popup" id="popupUndismissible" class="ui-content" style="margin-left: 230px;margin-left: 230px;width: 100%;height: 50px;border-top-width: 0px;padding-top: 8px" data-dismissible="false">
-					    <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-left">Fermer</a>
-					    <form>
-					    	<label for="slider-1">Volets :</label>
-					    	<input type="range" name="slider-1" id="slider-1" min="0" max="100" value="50">
-					    </form>
-					</div>
 
-					<div class="bascarrevolets">
-						<div  data-inline="true">
-							<img id="imagevoletsbouton" src="images/volets-ouvert.png">
-							<p id="tailletextevolets">Ouverture/Fermeture</p>
-							<a href=""><img class="imageflechehaut" type="image" src="images/flechehaut.png"></a>
-							<a href=""><img class="imagefleche" type="image" src="images/stop.png"></a>
-							<a href=""><img class="imagefleche" type="image" src="images/flechebas.png"></a>
-						</div>
-					</div>
-				</div>
-				
 				<!--     	MODULE DE LA GESTION DES RADIATEURS		 -->
 
 				<div id ="carrecentralbas">
